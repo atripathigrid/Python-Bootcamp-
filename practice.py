3f1bae5314938b5c18045e85de163b5072a4271f
atripathigrid <actripathi@griddynamics.com>
2026-02-02 12:57:11 +0530

    another update

--- practice.py
+++ practice.py
@@ -12,5 +12,47 @@
 #Difference between floating point and integers
 #Integers has no decimal points in it but floating points are the numbers which has decomal points in it.
 
-a = 30.5
-type(a)
+# String Properties and Methods 
+#Immutability of Strings - You cannot mutate or you cannot change.
+#Concatinatng two strings
+name = 'Tim'
+last_letters = name[1:]
+last_letters
+'s' + last_letters
+
+x = 'Good Morning'
+x = x + " I am Achintya Tripathi."
+x
+
+#String Formatting for printing
+#There are 2 methods of string formatting 
+# .format() method
+# f-strings (formatted string literals)
+
+# Formatting with .format() method
+#A good way to format objects into your strings for print statements is with the strings .format method
+# The syntax is:
+print('Hi Achintya Tripathi this {}' .format('side'))
+print('Hi Achintya {} {} {}' . format ('Tripathi','this' ,'side'))
+print('The {} {} {}' .format ('fox', 'brown', 'quick'))
+print('The {2} {1} {0}' .format ('fox', 'brown', 'quick'))
+
+# Float formatting with .format() method
+# The syntax is: "value:width.precision f"
+result = 100/333
+result
+print('The result is {}' .format(result))
+print('The result is {r}' .format(r = result))
+print('The result is {r:1.3f}' .format(r = result))
+print('The result is {r:1.6f}' .format(r = result))
+
+# F-strings (Formatted Strings Literals)
+name = "Achintya Tripathi"
+name 
+print ('Hi, my name is {}' .format(name)) #This was .format formatting
+print(f'Hi, my name is {name}') #f-strings formatting
+name = "Achintya Tripathi"
+age = 23
+print(f'Hi, my name is {name} and I am {age} years old') #f-strings formatting with multiple strings
+
+
