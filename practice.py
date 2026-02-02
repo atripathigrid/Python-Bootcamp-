2d5c8eb71d8d94f87996f40805a29390dd65b531
atripathigrid <actripathi@griddynamics.com>
2026-02-02 12:50:23 +0530

    Python practice of Concatenation and formatting strings

---
+++ practice.py
@@ -0,0 +1,16 @@
+#Python Practice 
+#DATA TYPES 
+#int - Integer (whole numbers like 3,400,5000)
+#float - Floating Point (Numbers with decimal point)
+#str - Strings (Ordered Sequence of a character ("Hello", "Achintya"))
+#list - Lists (Ordered sequence of objects [10, "Hi", 200.4]) 
+#dict - Dictionaries (Unordered key:value pairs {"myname":"value", "name":"Achintya" })
+#tup - Tuples (Ordered IMmmutable sequence of objects (10,"Achintya",100.5))
+#set - Sets (Unordered collection of unique objects {"E", "F"})
+#bool - Booleans (Logical value indicates True or False)
+
+#Difference between floating point and integers
+#Integers has no decimal points in it but floating points are the numbers which has decomal points in it.
+
+a = 30.5
+type(a)
