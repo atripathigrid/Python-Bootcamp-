6f7171acf04f8527c172715c460002e252836432
atripathigrid <actripathi@griddynamics.com>
2026-02-03 12:09:25 +0530

    Boolean Practice

---
+++ Booleans.py
@@ -0,0 +1,8 @@
+#BOOLEANS
+#Booleans are the operator that allows you to convey the TRUE or FAlSE statements.
+True
+False
+type(True)
+1>2
+1 == 1 #These equal signs are called comparison operators.
+
