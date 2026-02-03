7dd2e003b2a6dbe4e95156102ac100ee47415a20
atripathigrid <actripathi@griddynamics.com>
2026-02-03 11:23:08 +0530

    Sets Practice

---
+++ sets.py
@@ -0,0 +1,10 @@
+#SETS 
+# Sets are unordered collections of unique elements which is like letter A cannot be there more than a one time.Meaning that there can be only one representative of the same object.
+myset = set()
+myset
+myset.add(1)
+myset
+myset.add(2)
+myset
+my_list = [1,1,1,2,2,2,3,3,3]
+set(my_list)
