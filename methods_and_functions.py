ef11970cccdbc77a4ebf0a858445899dac44bf4e
atripathigrid <actripathi@griddynamics.com>
2026-02-17 11:23:00 +0530

    Methods and Fuunction

--- methods_and_functions.py
+++ methods_and_functions.py
@@ -82,6 +82,27 @@ for ticker,price in stock_prices:
     print(price)
 
 
+#Working on working hours 
+work_hours = [('Aman',150),('Shubham',90),('Prashant',350),('Deepak',260)]
+
+def employee_check(work_hours):
+
+    current_max = 0
+    employee_of_month = ''
+
+    for employee,hours in work_hours:
+        if hours > current_max:
+            current_max = hours 
+            employee_of_month = employee 
+        else:
+            pass
+
+
+    #Return
+    return(employee_of_month,current_max)
+
+employee_check(work_hours)
+
 
 
 
